7933df3945c970921b6cd1728d4219a040a7f87a
vaqif mohbaliyev <vagifmohbaliyev45@gmail.com>
2026-08-02 21:19:54 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'tse4.mm.bing.net',
       },
+      {
+        protocol: 'https',
+        hostname: '*.gstatic.com',
+      },
     ],
   },
 };
